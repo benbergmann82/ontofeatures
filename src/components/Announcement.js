22acc445b2175463308454561281cdccbd69527d
Ben Bergmann <ben@benbergmann.dev>
2021-09-14 15:36:17 +0100

    fixed a mobile bug

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Announcement = ({text}) => {
     return (
-        <div className="w-full py-2 flex justify-center items-center bg-green-600 text-white">
+        <div className="w-full py-2 text-center flex justify-center items-center bg-green-600 text-white">
             {text}
         </div>
     )
