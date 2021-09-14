ef4aaf7b0b71119a6023fd8825fc558d9d2f06ca
Ben Bergmann <ben@benbergmann.dev>
2021-09-14 10:08:00 +0100

    Fixed bugs

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,17 +1,15 @@
 import React from 'react';
-// import { ReactComponent as Cross } from '../svg/cross.svg';
-// import { ReactComponent as Check } from '../svg/check.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Feature = ({name, features}) => {
+    const cross = `${process.env.PUBLIC_URL}/assets/images/cross.svg`;
+    const check = `${process.env.PUBLIC_URL}/assets/images/check.svg`;
+
     const items = Object.keys(features).map(title => {
         if(typeof features[title] === 'boolean') {
             return (
                 <div className="flex justify-between py-1 even:bg-gray-200">
                     <div>{title}</div>
-                    <div>{features[title] ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faTimes} /> }</div>
+                    <div>{features[title] ? <img className="w-5 h-5" src={check} /> : <img className="w-5 h-5" src={cross} /> }</div>
                 </div>
             )
         } else {
