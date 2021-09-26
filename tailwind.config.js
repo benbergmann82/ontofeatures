de981fee69b936ac15707f487728d74ceefa5bee
Ben Bergmann <ben@benbergmann.dev>
2021-09-26 18:37:03 +0100

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '96': '28rem',
+      }
+    },
   },
   variants: {
     extend: {
       backgroundColor: ['even'],
+      borderWidth: ['last'],
     },
   },
   plugins: [],
