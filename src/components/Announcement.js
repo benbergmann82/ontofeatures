a9c6534258ec51d03fd64030e0cf7aaa3cbb0f64
Ben Bergmann <88056405+benbergmann82@users.noreply.github.com>
2022-04-22 18:40:15 +0100

    Update Announcement.js

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Announcement = () => {
     return (
         <div className="w-full py-4 px-2 mb-4 font-Roboto text-center bg-blue-500 text-gray-900 text-lg">
-            If you use my code <span className="mx-1 font-bold text-xl text-white">15cb9</span> when signing up, you'll get <span className="mx-1 font-bold text-xl text-white">£50 off your first month's subscription</span> from Onto!
+            If you use my code <span className="mx-1 font-bold text-xl text-white">newcode012</span> when signing up, you'll get <span className="mx-1 font-bold text-xl text-white">£50 off your first month's subscription</span> from Onto!
         </div>
     )
 }
